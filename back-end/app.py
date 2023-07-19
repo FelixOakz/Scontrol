c7245b0ed1c2a7c3e0151001c31258053185aa6f
felixoakz <felix@oakz.org>
2023-07-19 12:06:25 -0300

    initiated backend with flasj

---
+++ back-end/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def get_data():
+    return jsonify({"data": {'hello': 'world'}})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)  # only add if in production
