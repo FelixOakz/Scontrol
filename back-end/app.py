536d40eb70bd7397ee8836efac8116c1d4f404bd
felixoakz <felix@oakz.org>
2023-07-22 10:40:40 -0300

    venv gitignore

--- back-end/app.py
+++ back-end/app.py
@@ -1,12 +1,12 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)  # Allow all origins; use CORS(app, origins='http://your-react-native-app.com') to specify a specific origin
 
 @app.route('/', methods=['GET'])
 def get_data():
     return jsonify({"data": {'hello': 'world'}})
 
-
 if __name__ == "__main__":
-    app.run(debug=True)  # only add if in production
+    app.run(debug=True)
