42ce793e6b50794ed46a5f7070401e242e498d92
felixoakz <felix@oakz.org>
2023-07-24 20:31:03 -0300

    deleted api test

--- front-end/src/Routes/Routes.js
+++ front-end/src/Routes/Routes.js
@@ -2,30 +2,28 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Welcome from "../pages/Welcome/Welcome";
 import SignIn from "../pages/SignIn/SignIn";
 import SignUp from "../pages/SignUp/SignUp";
-import ApiTest from "../pages/ApiTest/apiTest";
 
 const Stack = createNativeStackNavigator();
 
 const Routes = () => {
   return (
-    <ApiTest />
-    // <Stack.Navigator>
-    //   <Stack.Screen
-    //     name="Welcome"
-    //     component={Welcome}
-    //     options={{ headerShown: false }}
-    //   />
-    //   <Stack.Screen
-    //     name="SignIn"
-    //     component={SignIn}
-    //     options={{ headerShown: false }}
-    //   />
-    //   <Stack.Screen
-    //     name="SignUp"
-    //     component={SignUp}
-    //     options={{ headerShown: false }}
-    //   />
-    // </Stack.Navigator>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Welcome"
+        component={Welcome}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="SignIn"
+        component={SignIn}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="SignUp"
+        component={SignUp}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
   );
 };
 
