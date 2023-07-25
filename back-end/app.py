7a65f062c23b7b2e0a943622156c531dea32aa7a
felixoakz <felix@oakz.org>
2023-07-25 20:57:21 -0300

    saving changes

--- back-end/app.py
+++ back-end/app.py
@@ -1,8 +1,6 @@
 from flask import Flask, jsonify
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Allow all origins; use CORS(app, origins='http://your-react-native-app.com') to specify a specific origin
 
 @app.route('/', methods=['GET'])
 def get_data():
