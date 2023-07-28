21c63e7e636243c236979f48aa77150a0b004dd9
felixoakz <felix@oakz.org>
2023-07-28 20:44:28 -0300

    implemented home navigation and some small changes

--- back-end/app.py
+++ back-end/app.py
@@ -1,10 +1,9 @@
 from flask import Flask, jsonify, request
-import helpers
 
 app = Flask(__name__)
 
 dummy_user = {
-    "email": "felix@oakz.org",
+    "email": "Felix",
     "password": "123",
 }
 
@@ -18,22 +17,26 @@ def test():
 def sign_in():
     try:
         data = request.json
-        email = request.get('email')
-        password = data.get('password')
+        email = data['email']
+        password = data['password']
+        print(data)
 
         if email == dummy_user["email"] and password == dummy_user["password"]:
-            return (jsonify({
-                "success": True, "message": "Sign-In successful!"
-            }))
+            return jsonify({
+                "success": True,
+                "message": "Sign-In successful!"
+            })
         else:
-            return (jsonify({
-                "success": False, "message": "Invalid credentials! Try again."
-            }))
+            return jsonify({
+                "success": False,
+                "message": "Invalid credentials! Try again."
+            })
     except Exception as e:
         print("Error during sign-in", e)
-        return (jsonify({
-            "success": False, "message": "An error ocurred during Sign-In"
-        }))
+        return jsonify({
+            "success": False,
+            "message": "An error occurred during Sign-In"
+        })
 
 
 if __name__ == "__main__":
